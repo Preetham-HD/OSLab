815ab0ab02c8ca61ea9fe3ebd135973996888ecc
Preetham-HD <preethamhd.cs23@bmsce.ac.in>
2025-03-12 10:37:54 +0530

    Update FcfsUsingArray.c

--- week1/FcfsUsingArray.c
+++ week1/FcfsUsingArray.c
@@ -36,3 +36,20 @@ void main() {
         printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
     }
 }
+/*
+OUTPUT:
+Enter the number of processes: 3
+
+Enter the arrival and burst time of each of the processes:
+P1 Arrival time: 1
+P1 Burst time: 2
+P2 Arrival time: 2
+P2 Burst time: 4
+P3 Arrival time: 4
+P3 Burst time: 5
+
+Process AT      BT      CT      TAT     WT      RT
+P1      1       2       3       2       0       0
+P2      2       4       7       5       1       1
+P3      4       5       12      8       3       3
+*/
